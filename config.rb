1b097feffabd0726d2f96d8203fd00695377c0f7
Jordan Isip <me@jordanisip.com>
2014-12-23 13:59:57 -0500

    Getting started on styling content and static voting buttons

--- config.rb
+++ config.rb
@@ -41,11 +41,11 @@
 # end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def voting_emotions
+    ["sad", "meh", "happy"]
+  end
+end
 
 set :css_dir, 'stylesheets'
 
