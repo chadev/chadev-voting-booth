decaeb3fe6ff4147b973b82e7716c8fa77894a19
Jordan Isip <me@jordanisip.com>
2015-01-15 11:32:48 -0500

    Removing duplicate function

--- source/javascripts/voting.js
+++ source/javascripts/voting.js
@@ -200,18 +200,6 @@ function getUrlParameter(sParam)
   }
 }
 
-function getUrlParameter(sParam) {
-  var sPageURL = window.location.search.substring(1);
-  var sURLVariables = sPageURL.split('&');
-  for (var i = 0; i < sURLVariables.length; i++)
-  {
-    var sParameterName = sURLVariables[i].split('=');
-    if (sParameterName[0] == sParam)
-    {
-      return sParameterName[1];
-    }
-  }
-}
 
 $(function() {
   FastClick.attach(document.body);
